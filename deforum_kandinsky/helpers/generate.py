1ea1e0bad297adb8eff8b5923abaf4f299902d14
Said Azizov <69383296+stazizov@users.noreply.github.com>
2024-01-22 14:17:03 +0300

    replaced interpolation with standart generation

--- deforum_kandinsky/helpers/generate.py
+++ deforum_kandinsky/helpers/generate.py
@@ -184,10 +184,17 @@ def generate(args, root, frame=0, return_latent=False, return_sample=False, retu
                         c = args.init_c
                     else:
                         # if isinstance(cond_prompts, Image.Image):
-                        c = root.model.prior.interpolate(cond_prompts, weights=[1], generator=generator).image_embeds
+                        c = root.model.prior(
+                            cond_prompts, 
+                            generator=generator
+                        ).image_embeds
                         # else:
                             # c = root.model.prior(cond_prompts, guidance_scale=1.0, generator=generator).image_embeds
-                    uc = root.model.prior(uncond_prompts, guidance_scale=1.0, generator=generator).negative_image_embeds
+                    uc = root.model.prior(
+                        uncond_prompts, 
+                        guidance_scale=1.0, 
+                        generator=generator
+                    ).negative_image_embeds
 
                     model_kwargs = dict(
                          image_embeds=c,
